4c470239e71cedbaa5641ddb04b040f2620a96d5
mizaelengersvargas <111417598+mizaelengersvargas@users.noreply.github.com>
2023-09-13 20:50:47 -0300

    1 file modified, 2 added, 1 deleted

--- src/components/Filme/Filme.tsx
+++ src/components/Filme/Filme.tsx
@@ -1,3 +1,5 @@
+import './filme.css';
+
 export interface FilmeInterface {
   original_title: string;
   release_date: string;
@@ -11,8 +13,10 @@ export interface FilmeProps {
 export function Filme({ filme }: FilmeProps) {
   return (
     <>
-      <img src={'https://image.tmdb.org/t/p/w154' + filme.poster_path} />
-      <div>{filme.original_title}</div>
+      <div className="filme">
+        <img src={'https://image.tmdb.org/t/p/w154/' + filme.poster_path} />
+        <div>{filme.original_title}</div>
+      </div>
     </>
   );
 }
