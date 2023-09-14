dbea8c4483e14a17816ab1ea340ef89faa97b0f0
mizaelengersvargas <111417598+mizaelengersvargas@users.noreply.github.com>
2023-09-13 21:16:35 -0300

    1 file modified

--- src/components/Filme/Filme.tsx
+++ src/components/Filme/Filme.tsx
@@ -4,6 +4,7 @@ export interface FilmeInterface {
   original_title: string;
   release_date: string;
   poster_path: string;
+  vote_average: number;
 }
 
 export interface FilmeProps {
@@ -17,6 +18,7 @@ export function Filme({ filme }: FilmeProps) {
         <img src={'https://image.tmdb.org/t/p/w154/' + filme.poster_path} />
         <div>{filme.original_title}</div>
         <div>{filme.release_date}</div>
+        <div>{filme.vote_average}</div>
       </div>
     </>
   );
