dc08e760120ff59219c7531f6a88a0780d27edf1
mizaelengersvargas <111417598+mizaelengersvargas@users.noreply.github.com>
2023-09-13 21:02:01 -0300

    1 file modified

--- src/components/Filme/Filme.tsx
+++ src/components/Filme/Filme.tsx
@@ -16,6 +16,7 @@ export function Filme({ filme }: FilmeProps) {
       <div className="filme">
         <img src={'https://image.tmdb.org/t/p/w154/' + filme.poster_path} />
         <div>{filme.original_title}</div>
+        <div>{filme.release_date}</div>
       </div>
     </>
   );
